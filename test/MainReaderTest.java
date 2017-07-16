5efcd766c60d3fa2645841b107a7e56a7d10dd16
cocomarinaru <g1tcoco>
2017-07-16 21:34:25 +0300

    change , commented , will refactor in future

--- test/MainReaderTest.java
+++ test/MainReaderTest.java
@@ -1,8 +1,8 @@
-import spy.reader.InputFileReader;
+//import spy.input.InputFileReader;
 
 public class MainReaderTest {
 
     public static void main(String[] args) {
-        InputFileReader.readInputFile("/home/ovidiu/cocos/spyfox/test_input/Articole noi.txt");
+//        InputFileReader.readInputFile("/home/ovidiu/cocos/spyfox/test_input/Articole noi.txt");
     }
 }
